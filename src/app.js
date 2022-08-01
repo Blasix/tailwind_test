841f7f40281ee7d4b6e045b6738add8bc873f8d7
BlasixPC <blasixcorp@gmail.com>
2022-08-01 21:04:27 +0200

    added dark mode (with tutorial) first commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+// Icons
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+// Theme vars
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Icon Toggling
+const iconToggle = () => {
+    moonIcon.classList.toggle("hidden");
+    sunIcon.classList.toggle("hidden");
+};
+// initial Theme check
+const themeCheck = () => {
+    if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark");
+        moonIcon.classList.add("hidden");
+        return;
+    }
+
+    sunIcon.classList.add("hidden");
+};
+
+// manual theme switch
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+};
+// call theme switch on clicking buttons
+sunIcon.addEventListener("click", () => {
+    themeSwitch();
+});
+
+moonIcon.addEventListener("click", () => {
+    themeSwitch();
+});
+
+// invoke theme check on initial load
+themeCheck();
